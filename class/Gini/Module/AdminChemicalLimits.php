3d92c8a7a19805c2dab92879c831476928ab0bc7
pihizi <pihizi@msn.com>
2016-06-24 03:00:08 +0000

    增加权限判断

--- class/Gini/Module/AdminChemicalLimits.php
+++ class/Gini/Module/AdminChemicalLimits.php
@@ -10,4 +10,22 @@ class AdminChemicalLimits
     public static function diagnose()
     {
     }
+
+    public static function commonACL($e, $user, $action, $project, $when, $where)
+    {
+        if (!$user->id) {
+            return false;
+        }
+
+        $group = _G('GROUP');
+        if ($user->isAdminOf($group)) {
+            return true;
+        }
+
+        if (self::_userHasPermission($user, 'admin')) {
+            return true;
+        }
+
+        return false;
+    }
 }
