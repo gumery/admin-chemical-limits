d71232e8111df62a9f67c4719a2703d594683182
pihizi <pihizi@msn.com>
2016-06-24 09:13:01 +0000

    增加排序

--- class/Gini/Controller/API/Admin/Inventory.php
+++ class/Gini/Controller/API/Admin/Inventory.php
@@ -101,7 +101,7 @@ class Inventory extends \Gini\Controller\API
         $group = a('group', $criteria['group_id']);
         if (!$group->id) return [];
 
-        $requests = those('inventory/request')->whose('group')->is($group)->limit($start, $perpage);
+        $requests = those('inventory/request')->whose('group')->is($group)->orderBy('mtime', 'desc')->orderBy('ctime', 'desc')->limit($start, $perpage);
 
         $result = [];
         foreach ($requests as $request) {
