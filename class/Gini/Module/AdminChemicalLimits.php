d20d13dee757d25a1d6a42a37437b8db4c608517
pihizi <pihizi@msn.com>
2016-06-28 05:04:25 +0000

    .

--- class/Gini/Module/AdminChemicalLimits.php
+++ class/Gini/Module/AdminChemicalLimits.php
@@ -11,6 +11,20 @@ class AdminChemicalLimits
     {
     }
 
+    private static function _userHasPermission($user, $perm)
+    {
+        static $_PERM_CACHE = [];
+        $group = _G('GROUP');
+        if (!isset($_PERM_CACHE[$perm])) {
+            $permission = a('user/permission', ['group' => $group, 'name' => $perm]);
+            foreach ($permission->users as $u) {
+                $_PERM_CACHE[$perm][$u->id] = true;
+            }
+        }
+
+        return (bool) $_PERM_CACHE[$perm][$user->id];
+    }
+
     public static function commonACL($e, $user, $action, $project, $when, $where)
     {
         if (!$user->id) {
