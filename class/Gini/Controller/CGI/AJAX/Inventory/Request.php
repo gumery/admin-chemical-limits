622e4e868c35f38044e4a3a2950fe94356027e96
pihizi <pihizi@msn.com>
2017-01-13 18:17:27 +0800

    添加通过拒绝的备注

--- class/Gini/Controller/CGI/AJAX/Inventory/Request.php
+++ class/Gini/Controller/CGI/AJAX/Inventory/Request.php
@@ -25,97 +25,134 @@ class Request extends \Gini\Controller\CGI
         ]));
     }
 
-    public function actionReject($requestID)
+    public function actionGetOPDialog()
     {
         $me = _G('ME');
         $group = _G('GROUP');
         if (!$me->id || !$group->id || !$me->isAllowedTo('设置存量上限')) return;
+        $form = $this->form();
+        $type = trim($form['type']);
+        $requestID = trim($form['id']);
+
+        $types = [
+            'approve'=> '批准申请', 
+            'reject'=> '拒绝申请'
+        ];
+        if (!in_array($type, array_keys($types))) return;
 
         $request = a('inventory/request', $requestID);
         if (!$request->id || $request->status!=\Gini\ORM\Inventory\Request::STATUS_PENDING) return;
 
-        $request->reject_time = $request->mtime = date('Y-m-d H:i:s');
-        $request->reject_man = $me;
-        $request->status = \Gini\ORM\Inventory\Request::STATUS_REJECTED;
-        $bool = $request->save();
-        return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
-            'code'=> $bool ? 0 : 1,
-            'message'=> $bool ? T('操作成功') : T('操作失败，请您重试')
-        ]);
+        return \Gini\IoC::construct('\Gini\CGI\Response\HTML', V('inventory/request-op-dialog', [
+            'type'=> $type,
+            'title'=> $types[$type],
+            'request'=> $request
+        ]));
     }
 
-    public function actionApprove($requestID)
+    public function actionPostOPRequest()
     {
         $me = _G('ME');
         $group = _G('GROUP');
         if (!$me->id || !$group->id || !$me->isAllowedTo('设置存量上限')) return;
 
+        $form = $this->form('post');
+        $type = trim($form['type']);
+        $requestID = trim($form['id']);
+        $note = trim($form['note']);
+
+        if ($note==='') return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
+            'code'=>  1,
+            'message'=> T('请填写备注信息')
+        ]);
+
         $request = a('inventory/request', $requestID);
         if (!$request->id || $request->status!=\Gini\ORM\Inventory\Request::STATUS_PENDING) return;
 
-        $db = \Gini\Database::db();
-        try {
-            $db->beginTransaction();
-            $request->approve_time = $request->mtime = date('Y-m-d H:i:s');
-            $request->approve_man = $me;
-            $request->status = \Gini\ORM\Inventory\Request::STATUS_APPROVED;
-            $bool = $request->save();
-            if (!$bool) throw new \Exception();
+        switch ($type) {
+        case 'approve':
+            $db = \Gini\Database::db();
+            try {
+                $db->beginTransaction();
+                $request->approve_time = $request->mtime = date('Y-m-d H:i:s');
+                $request->approve_man = $me;
+                $request->approve_note = $note;
+                $request->status = \Gini\ORM\Inventory\Request::STATUS_APPROVED;
+                $bool = $request->save();
+                if (!$bool) throw new \Exception();
 
-            if ($request->cas_no) {
-                $chemicalInfo = \Gini\ChemDB\Client::getChemicalInfo($request->cas_no);
-                if (empty($chemicalInfo)) {
-                    throw new \Exception();
+                if ($request->cas_no) {
+                    $chemicalInfo = \Gini\ChemDB\Client::getChemicalInfo($request->cas_no);
+                    if (empty($chemicalInfo)) {
+                        throw new \Exception();
+                    }
                 }
-            }
 
-            $pa = those('inventory/reagent')->whose('cas_no')->is($request->cas_no ?: $request->type)->andWhose('group_id')->is(null)->current();
-            if (!$pa->id) {
-                $pa = a('inventory/reagent');
-                $pa->cas_no = $request->cas_no ?: $request->type;
+                $pa = those('inventory/reagent')->whose('cas_no')->is($request->cas_no ?: $request->type)->andWhose('group_id')->is(null)->current();
+                if (!$pa->id) {
+                    $pa = a('inventory/reagent');
+                    $pa->cas_no = $request->cas_no ?: $request->type;
+                    if (!empty($chemicalInfo)) {
+                        $pa->name = $chemicalInfo['name'];
+                        $pa->types = implode(',', $chemicalInfo['types']);
+                        $pa->state = $chemicalInfo['state'];
+                    }
+                    if (!$pa->save()) {
+                        throw new \Exception();
+                    }
+                }
+
+                $reagent = a('inventory/reagent', [
+                    'cas_no'=> $request->cas_no ?: $request->type,
+                    'group'=> $request->group
+                ]);
+
+                if (!$reagent->id) {
+                    $reagent->group = $request->group;
+                    $reagent->cas_no = $request->cas_no ?: $request->type;
+                }
+
                 if (!empty($chemicalInfo)) {
-                    $pa->name = $chemicalInfo['name'];
-                    $pa->types = implode(',', $chemicalInfo['types']);
-                    $pa->state = $chemicalInfo['state'];
+                    $reagent->name = $chemicalInfo['name'];
+                    $reagent->types = implode(',', $chemicalInfo['types']);
+                    $reagent->state = $chemicalInfo['state'];
                 }
-                if (!$pa->save()) {
-                    throw new \Exception();
-                }
+
+                $reagent->volume = $request->volume;
+                $bool = $reagent->save();
+                if (!$bool) throw new \Exception();
+
+                $db->commit();
+                return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
+                    'code'=> 0,
+                    'message'=> T('修改成功'),
+                    'id'=> $request->id,
+                    'text'=> T('已通过'),
+                ]);
             }
-
-            $reagent = a('inventory/reagent', [
-                'cas_no'=> $request->cas_no ?: $request->type,
-                'group'=> $request->group
-            ]);
-
-            if (!$reagent->id) {
-                $reagent->group = $request->group;
-                $reagent->cas_no = $request->cas_no ?: $request->type;
+            catch (\Exception $e) {
+                $db->rollback();
             }
-
-            if (!empty($chemicalInfo)) {
-                $reagent->name = $chemicalInfo['name'];
-                $reagent->types = implode(',', $chemicalInfo['types']);
-                $reagent->state = $chemicalInfo['state'];
-            }
-
-            $reagent->volume = $request->volume;
-            $bool = $reagent->save();
-            if (!$bool) throw new \Exception();
-
-            $db->commit();
             return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
-                'code'=> 0,
-                'message'=> T('修改成功')
+                'code'=> 1,
+                'message'=> T('操作失败，请您重试')
             ]);
+            break;
+        case 'reject':
+            $request->reject_time = $request->mtime = date('Y-m-d H:i:s');
+            $request->reject_man = $me;
+            $request->status = \Gini\ORM\Inventory\Request::STATUS_REJECTED;
+            $request->reject_note = $note;
+            $bool = $request->save();
+            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
+                'code'=> $bool ? 0 : 1,
+                'message'=> $bool ? T('操作成功') : T('操作失败，请您重试'),
+                'id'=> $bool ? $request->id : 0,
+                'text'=> $bool ? T('已拒绝') : '',
+            ]);
+            break;
+        default:
         }
-        catch (\Exception $e) {
-            $db->rollback();
-        }
-        return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
-            'code'=> 1,
-            'message'=> T('操作失败，请您重试')
-        ]);
     }
 }
 
--- raw/assets/js/page/admin/chemical/limits/operate-request.js
+++ raw/assets/js/page/admin/chemical/limits/operate-request.js
@@ -1,37 +1,74 @@
 define('page/admin/chemical/limits/operate-request', ['jquery', 'bootbox'], function($, Bootbox) {
-    function op(type, id) {
-        var $content = $('.app-handler-content');
-        var message, text, url;
-        if (type == 'approve') {
-            message = $content.attr('data-approve-message');
-            text = $content.attr('data-approved-text');
-            url = $content.attr('data-approve-url');
-        } else {
-            message = $content.attr('data-reject-message');
-            text = $content.attr('data-rejected-text');
-            url = $content.attr('data-reject-url');
+
+    var $modalLoading;
+    function showLoading() {
+        if ($modalLoading) {
+            $modalLoading.modal({
+                'backdrop': 'static',
+                'show': true
+            });
+            return;
         }
-        Bootbox.confirm(message, function(bool) {
-            if (!bool) return;
-            $.post([url, id].join('/'), function(data) {
-                data = data || {};
-                if (data.code) {
-                    Bootbox.alert(data.message);
-                    return;
-                }
-                $(['.app-handler-container[data-id=', id, ']'].join('')).text(text);
+        var loading = [
+            '<div class="modal"><div class="modal-content"><div class="modal-body">',
+                '<div class="text-center">', 
+                    '<i class="fa fa-2x fa-spinner fa-spin"/>', 
+                '</div>',
+            '</div></div></div>'
+        ].join('\n');
+        $modalLoading = $(loading);
+        showLoading();
+    }
+
+    function hideLoading() {
+        if ($modalLoading) {
+            $modalLoading.modal('hide');
+        }
+    }
+
+    function op(type, id) {
+        showLoading();
+        var url = 'ajax/inventory/request/get-op-dialog';
+        $.get(url, {
+            'type': type,
+            'id': id
+        }, function (result) {
+            hideLoading();
+            $(result).modal({
+                'backdrop': 'static',
+                'show': true
             });
         });
     }
+
+    $(document).on('submit', '.form-op-volume-request', function() {
+        var $form = $(this);
+        var url = 'ajax/inventory/request/post-op-request';
+        showLoading();
+        $.post(url, $form.serialize(), function(response) {
+            hideLoading();
+            response = response || {};
+            if (response.code) {
+                Bootbox.alert(response.message);
+                return;
+            }
+            if (response.id) {
+                $(['.app-handler-container[data-id=', response.id, ']'].join('')).text(response.text);
+            }
+        });
+    });
+
     $(document).on('click', '.app-handler-approve-request', function(data) {
         var id = $(this).parents('.app-handler-container').attr('data-id');
         op('approve', id);
         return false;
     });
+
     $(document).on('click', '.app-handler-reject-request', function(data) {
         var id = $(this).parents('.app-handler-container').attr('data-id');
         op('reject', id);
         return false;
     });
+
 });
 
